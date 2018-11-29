a5cb311d9d7e358f58c942a48b1296974eec809e
Mehdi Boukhechba <mob3f@sys.virginia.edu>
2018-11-29 15:04:01 -0500

    commit

--- Mixed effect model.R
+++ Mixed effect model.R
@@ -13,10 +13,10 @@ library(ggplot2)
 writeresults <- function(x,path){
   print(paste0("Current working dir: ",path))
   sink(file=path) 
-  print(summary(lme(posExpBiasScale ~ condition, random = ~1+session_int|participantId,control=ctrl, data=z, method="ML")))
+  print(summary(lme(posExpBiasScale ~ condition*session_int, random = ~1+session_int|participantId,control=ctrl, data=z, method="ML")))
   print(summary(lme(negExpBiasScale ~ condition*session_int, random = ~1+session_int|participantId,control=ctrl, data=x, method="ML")))
   #Delete imputed data for non measured sessions
-  y <- subset(x, session_int!=2 & session_int!=4)
+  y <- subset(x, session_int!=1 & session_int!=3)
   unique(y$session_int)
   print(summary(lme(depressionScale ~ condition*session_int, random = ~1+session_int|participantId,control=ctrl, data=y, method="ML")))
   print(summary(lme(anxietyScale ~ condition*session_int, random = ~1+session_int|participantId,control=ctrl, data=y, method="ML")))
@@ -31,7 +31,7 @@ writeresults2 <- function(x,path){
   print(summary(lme(posExpBiasScale ~ session_int, random = ~1+session_int|participantId,control=ctrl, data=x, method="ML")))
   print(summary(lme(negExpBiasScale ~ session_int, random = ~1+session_int|participantId,control=ctrl, data=x, method="ML")))
   #Delete imputed data for non measured sessions
-  y <- subset(x, session_int!=2 & session_int!=4)
+  y <- subset(x, session_int!=1 & session_int!=3)
   unique(y$session_int)
   print(summary(lme(depressionScale ~ session_int, random = ~1+session_int|participantId,control=ctrl, data=y, method="ML")))
   print(summary(lme(anxietyScale ~ session_int, random = ~1+session_int|participantId,control=ctrl, data=y, method="ML")))
@@ -49,6 +49,7 @@ preparedata<-function(x){
   x$session <- gsub('preTest','Baseline',  x$session)
   x$session <- factor(x$session, levels=c("Baseline", "firstSession","secondSession","thirdSession","fourthSession","PostFollowUp"))
   x$session_int <- as.integer(x$session)
+  x$session_int <-x$session_int-1
   x<-aggregate(x=x[c("posExpBiasScale", "negExpBiasScale","depressionScale","anxietyScale","selfEffScale","growthMindScale","optimismScale")], by=list(participantId=x$participantId,session=x$session,condition=x$condition,session_int=x$session_int), mean, na.rm = TRUE)
   is.nan.data.frame <- function(x)
     do.call(cbind, lapply(x, is.nan))
@@ -87,7 +88,7 @@ impx <- mitmlComplete(imp, 1)
 #ITT
 ittx<-impx[which(impx$participantId %in% ITT$participantId), ]
 # Treament phase
-z <- subset(ittx, session_int!=6)
+z <- subset(ittx, session_int!=5)
 
 z$condition <- factor(z$condition, levels=c( "NEUTRAL","FIFTYFIFTY","POSITIVE"))
 writeresults(z,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Treatment phase/ITT/2conditions_vs_Neutral.txt')
@@ -105,7 +106,7 @@ c <- subset(z, condition=="POSITIVE")
 writeresults2(c,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Treatment phase/ITT/POSITIVE.txt')
 
 # Follow-up phase
-y <- subset(ittx, session_int==5 |session_int==6)
+y <- subset(ittx, session_int==4 |session_int==5)
 y$condition <- factor(y$condition, levels=c( "NEUTRAL","FIFTYFIFTY","POSITIVE"))
 writeresults(y,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Follow-up phase/ITT/2conditions_vs_Neutral.txt')
 
@@ -125,7 +126,7 @@ writeresults2(c,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Lo
 compx<-impx[which(impx$participantId %in% completers$participantId), ]
 
 # Treament phase
-z <- subset(compx, session_int!=6)
+z <- subset(compx, session_int!=5)
 
 z$condition <- factor(z$condition, levels=c( "NEUTRAL","FIFTYFIFTY","POSITIVE"))
 writeresults(z,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Treatment phase/Completers/2conditions_vs_Neutral.txt')
@@ -143,7 +144,7 @@ c <- subset(z, condition=="POSITIVE")
 writeresults2(c,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Treatment phase/Completers/POSITIVE.txt')
 
 # Follow-up phase
-y <- subset(compx, session_int==5 |session_int==6)
+y <- subset(compx, session_int==4 |session_int==5)
 y$condition <- factor(y$condition, levels=c( "NEUTRAL","FIFTYFIFTY","POSITIVE"))
 writeresults(y,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Follow-up phase/Completers/2conditions_vs_Neutral.txt')
 
@@ -235,98 +236,51 @@ atrisk<-new[which(new$ratio< -0.5 ),]
 #summary(imp)
 #plot(imp, trace="all", print="beta", pos=c(1,2))
 
-#specifying contrast
-C1 <- c(-180/961,814/961,-146/961,-173/961,-315/961)
-C2 <- c(-180/961,-147/961,815/961,-173/961,-315/961)
-C3 <- c(-180/961,-147/961,-146/961,788/961,-315/961)
-C4 <- c(-180/961,-147/961,-146/961,-173/961,646/961)
-contrs <- cbind(C1,C2,C3,C4)
+# #specifying contrast
+# C1 <- c(-180/961,814/961,-146/961,-173/961,-315/961)
+# C2 <- c(-180/961,-147/961,815/961,-173/961,-315/961)
+# C3 <- c(-180/961,-147/961,-146/961,788/961,-315/961)
+# C4 <- c(-180/961,-147/961,-146/961,-173/961,646/961)
+# contrs <- cbind(C1,C2,C3,C4)
+# 
+# C0 <- c(1/5,1/5,1/5,1/5,1/5)
+# pos_half <- c(-1/2,-1/2,1/2,1/2,0)
+# ran_bloc <- c(0,0,-1/2,1/2,0)
+# pp_npp <- c(-1/2,1/2,0,0,0)
+# pos_neut <- c(1/2,1/2,0,0,-1)
+# half_neut <- c(0,0,1/2,1/2,-1)
+# contrs2 <- cbind(pos_half,ran_bloc,pp_npp,pos_neut,half_neut)
+# 
+# 
+# pos_neut <- c((180+147)/(180+147+315),(180+147)/(180+147+315),0,0,-315/(180+147+315))
+# pos_half <- c((180+147)/(180+147+146+173),(180+147)/(180+147+146+173),-(146+173)/(180+147+146+173),-(146+173)/(180+147+146+173),0)
+# half_neut <- c(0,0,(146+173)/(146+173+315),(146+173)/(146+173+315),-315/(146+173+315))
+# bloc_ran <- c(0,0,146/(146+173),-173/(146+173),0)
+# pp_npp <- c(180/(180+147),-147/(180+147),0,0,0)
+# contrs5 <- cbind(pos_neut,pos_half,half_neut,half_neut,half_neut)
+# contrs6 <- cbind(bloc_ran,pp_npp )
+# 
+# C1 <- c(-1/5,-1/5,4/5,-1/5,-1/5)
+# C2 <- c(-1/5,-1/5,-1/5,4/5,-1/5)
+# C3 <- c(-1/5,-1/5,-1/5,-1/5,4/5)
+# C4 <- c(4/5,-1/5,-1/5,-1/5,-1/5)
+# contrs3 <- cbind(C1,C2,C3,C4)
 
-C0 <- c(1/5,1/5,1/5,1/5,1/5)
-pos_half <- c(-1/2,-1/2,1/2,1/2,0)
-ran_bloc <- c(0,0,-1/2,1/2,0)
-pp_npp <- c(-1/2,1/2,0,0,0)
-pos_neut <- c(1/2,1/2,0,0,-1)
-half_neut <- c(0,0,1/2,1/2,-1)
-contrs2 <- cbind(pos_half,ran_bloc,pp_npp,pos_neut,half_neut)
+#contrasts(z$condition) <-contrs5
 
-c0<-
-pos_neut <- c((180+147)/(180+147+315),(180+147)/(180+147+315),0,0,-315/(180+147+315))
-pos_half <- c((180+147)/(180+147+146+173),(180+147)/(180+147+146+173),-(146+173)/(180+147+146+173),-(146+173)/(180+147+146+173),0)
-half_neut <- c(0,0,(146+173)/(146+173+315),(146+173)/(146+173+315),-315/(146+173+315))
-bloc_ran <- c(0,0,146/(146+173),-173/(146+173),0)
-pp_npp <- c(180/(180+147),-147/(180+147),0,0,0)
-contrs5 <- cbind(pos_neut,pos_half,half_neut,half_neut,half_neut)
-contrs6 <- cbind(bloc_ran,pp_npp )
+# a<-do.call(data.frame,aggregate(posExpBiasScale~condition,data=z[which(z$session_int==0),],FUN=function(x) c(n = length(x),mean=mean(x),sd=sd(x))))
+# do.call(data.frame,aggregate(posExpBiasScale~condition,data=z,FUN=function(x) c(n = length(x),mean=mean(x),sd=sd(x))))
+# mean(z[which(z$session_int==0),"posExpBiasScale"])
+# do.call(data.frame,aggregate(posExpBiasScale.mean~condition,data=a,FUN=function(x) c(n = length(x)/5,mean=mean(x),sd=sd(x))))
 
-C1 <- c(-1/5,-1/5,4/5,-1/5,-1/5)
-C2 <- c(-1/5,-1/5,-1/5,4/5,-1/5)
-C3 <- c(-1/5,-1/5,-1/5,-1/5,4/5)
-C4 <- c(4/5,-1/5,-1/5,-1/5,-1/5)
-contrs3 <- cbind(C1,C2,C3,C4)
 
-contrasts(z$condition) <-contrs5
-solve(t(contrs2))
-
-participantId
-a<-do.call(data.frame,aggregate(posExpBiasScale~condition,data=z[which(z$session_int==0),],FUN=function(x) c(n = length(x),mean=mean(x),sd=sd(x))))
-do.call(data.frame,aggregate(posExpBiasScale~condition,data=z,FUN=function(x) c(n = length(x),mean=mean(x),sd=sd(x))))
-mean(z[which(z$session_int==0),"posExpBiasScale"])
-do.call(data.frame,aggregate(posExpBiasScale.mean~condition,data=a,FUN=function(x) c(n = length(x)/5,mean=mean(x),sd=sd(x))))
 #ITT
 ittx<-impx[which(impx$participantId %in% ITT$participantId), ]
-mean(z$posExpBiasScale)
 # Treament phase
-z <- subset(ittx, session_int!=6)
-
-mean<-(3.443493*(146/961)+3.244220*(173/961)+3.269841*(315/961)+3.425000*(180/961)+3.335034*(147/961))
-mean<-(3.443493+3.244220+3.269841+3.425000+3.335034)/5
-print(summary(lme(posExpBiasScale ~ condition*session_int, random = ~1+session_int|participantId, data=z, method="ML")))
-print(anova(lme(posExpBiasScale ~ condition*session_int, random = ~1+session_int|participantId, data=z, method="ML")))
-print(summary(lm(posExpBiasScale ~ condition*session_int, data=z)))
-table(z$session_int)
-print(summary(lmer(posExpBiasScale ~ condition*session_int + (1 +session_int|participantId), data = z,REML = FALSE)))
-ittx$session_int <-ittx$session_int-1
-z$session_int <- as.factor(z$session_int)
-z$session_int <- as.ordered(z$session_int)
-z$session_int <- as.integer(z$session_int)
-unique(z$session_int)
-contrasts(z$session_int)
-z$session_int <-z$session_int-1
-mean(z$posExpBiasScale)
-
-a<-z$session_int
-b<-rep(1,961*5)
-X<-data.frame(a,b)
-dim(X)
-X<-as.matrix(X)
-solve(t(X)%*%X)%*%t(X)%*%z$posExpBiasScale
-
-z$session_int<-as.factor(z$session_int)
-contrasts(z$session_int)
-a<-rep(1,961*5)
-b<-c(rep(0,961),rep(1,961),rep(0,961*3))
-c<-c(rep(0,961*2),rep(1,961),rep(0,961*2))
-d<-c(rep(0,961*3),rep(1,961),rep(0,961))
-e<-c(rep(0,961*4),rep(1,961))
-X<-data.frame(a,b,c,d,e)
-X<-as.matrix(X)
+z <- subset(ittx, session_int!=5)
 
 
-solve(t(X)%*%X)%*%t(X)%*%z$posExpBiasScale
-
-mean(z[which(z$session_int==2),"posExpBiasScale"])
-mean(ittx[which(ittx$session_int==0),"posExpBiasScale"])
-table(x$session_int)
-
-unique(z$session_int)
-z$condition <- factor(z$condition, levels=c("POSITIVE","POSITIVE_NEGATION","FIFTY_FIFTY_BLOCKED","FIFTY_FIFTY_RANDOM","NEUTRAL"))
-z$condition <- factor(z$condition, levels=c("POSITIVE_NEGATION","FIFTY_FIFTY_BLOCKED","FIFTY_FIFTY_RANDOM","NEUTRAL","POSITIVE"))
-contrasts(z$condition) <-contrs
-contrasts(z$condition) <-contrs2
-solve(t(contrs2))
-contrasts(z$condition) <-contrs3
-
+z$condition <- factor(z$condition, levels=c("FIFTY_FIFTY_BLOCKED","FIFTY_FIFTY_RANDOM","NEUTRAL","POSITIVE_NEGATION","POSITIVE"))
 writeresults(z,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Treatment phase/ITT/4conditions_vs_50_50Random.txt')
 
 z$condition <- factor(z$condition, levels=c("POSITIVE","POSITIVE_NEGATION","NEUTRAL","FIFTY_FIFTY_RANDOM","FIFTY_FIFTY_BLOCKED"))
@@ -348,14 +302,11 @@ c <- subset(z, condition=="FIFTY_FIFTY_BLOCKED")
 writeresults2(c,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Treatment phase/ITT/FIFTY_FIFTY_BLOCKED.txt')
 
 # Follow-up phase
-y <- subset(ittx, session_int==5 |session_int==6)
+y <- subset(ittx, session_int==4 |session_int==5)
 
 y$condition <- factor(y$condition, levels=c("FIFTY_FIFTY_RANDOM","FIFTY_FIFTY_BLOCKED","POSITIVE_NEGATION","POSITIVE","NEUTRAL"))
 writeresults(y,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Follow-up phase/ITT/4conditions_vs_50_50Random.txt')
 
-c <- subset(y, condition=="FIFTY_FIFTY_BLOCKED"|condition=="FIFTY_FIFTY_RANDOM")
-c$condition <- factor(y$condition, levels=c("FIFTY_FIFTY_BLOCKED","FIFTY_FIFTY_RANDOM"))
-writeresults(c,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Follow-up phase/ITT/50_50_blocked_vs_50_50Random.txt')
 
 y$condition <- factor(y$condition, levels=c("POSITIVE","POSITIVE_NEGATION","NEUTRAL","FIFTY_FIFTY_RANDOM","FIFTY_FIFTY_BLOCKED"))
 writeresults(y,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Follow-up phase/ITT/4conditions_vs_Positive.txt')
@@ -379,7 +330,7 @@ writeresults2(c,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Lo
 #Completers
 compx<-impx[which(impx$participantId %in% completers$participantId), ]
 # Treament phase
-z <- subset(compx, session_int!=6)
+z <- subset(compx, session_int!=5)
 
 z$condition <- factor(z$condition, levels=c("FIFTY_FIFTY_RANDOM","FIFTY_FIFTY_BLOCKED","POSITIVE_NEGATION","POSITIVE","NEUTRAL"))
 writeresults(z,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Treatment phase/Completers/4conditions_vs_50_50Random.txt')
@@ -400,7 +351,7 @@ c <- subset(z, condition=="FIFTY_FIFTY_BLOCKED")
 writeresults2(c,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Treatment phase/Completers/FIFTY_FIFTY_BLOCKED.txt')
 
 # Follow-up phase
-y <- subset(compx, session_int==5 |session_int==6)
+y <- subset(compx, session_int==4 |session_int==5)
 
 y$condition <- factor(y$condition, levels=c("FIFTY_FIFTY_RANDOM","FIFTY_FIFTY_BLOCKED","POSITIVE_NEGATION","POSITIVE","NEUTRAL"))
 writeresults(y,'C://Users/mob3f/Documents/MindTrials Future Thinking/results/Longitudinal Outcome/Follow-up phase/Completers/4conditions_vs_50_50Random.txt')
