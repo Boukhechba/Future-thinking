b3821aee29b1cec54f17b919009d1d950437d99d
mob3f <mob3f@virginia.edu>
2019-04-30 23:22:03 -0400

    commit

--- Mixed effect model-assumptions-check.R
+++ Mixed effect model-assumptions-check.R
@@ -118,50 +118,17 @@ ittx<-x[which(x$participantId %in% ITT$participantId), ]
 
 x<-ittx
 
+d<-read.csv("C://Users/mob3f/Documents/MindTrials Future Thinking/Clean data from Jeremy/FTmainDataDemogITT.csv")
+x<-merge(d[,-c(3,4)],x,by="participantId")
+
+
 #Testing MCAR
 
-#Testing MCAR with demographics
-d<-read.csv("C://Users/mob3f/Documents/MindTrials Future Thinking/Clean data from Jeremy/FTmainDataDemogITT.csv")
-total<-merge(d,x,by="participantId")
 
-
-
-total$posind<-as.integer(as.logical(is.na(total$posExpBiasScale)))
-total$negind<-as.integer(as.logical(is.na(total$negExpBiasScale)))
-total$depind<-as.integer(as.logical(is.na(total$depressionScale)))
-total$anxind<-as.integer(as.logical(is.na(total$anxietyScale)))
-total$selfind<-as.integer(as.logical(is.na(total$selfEffScale)))
-total$growind<-as.integer(as.logical(is.na(total$growthMindScale)))
-total$optind<-as.integer(as.logical(is.na(total$optimismScale)))
-
-total2<- aggregate(posind~participantId+age+genderId+race+ethnicity+maritalStat+education+educationGrp+employmentStat+employmentStatGrp+income+country+countryGrp,data=total,FUN=function(x) c(sum=sum(x)))
-
-
-sink(file="C:/Users/mob3f/Documents/MindTrials Future Thinking/results/testing_MCAR.txt")
-#age
-cor.test(total2$posind, total2$age, method = c("pearson"))
-#other demographics 
-summary( aov(posind ~ genderId, data = total2))
-summary( aov(posind ~ race, data = total2))
-summary( aov(posind ~ maritalStat, data = total2))
-summary( aov(posind ~ education, data = total2))
-summary( aov(posind ~ educationGrp, data = total2))
-summary( aov(posind ~ employmentStat, data = total2))
-summary( aov(posind ~ employmentStatGrp, data = total2))
-summary( aov(posind ~ income, data = total2))
-summary( aov(posind ~ country, data = total2))
-summary( aov(posind ~ countryGrp, data = total2))
-
-sink()
-
-
-total<-total[c(-1,-2,-3,-4)]
-c<-cor(total,use = "pairwise.complete.obs")
-write.csv(c, "C:/Users/mob3f/Documents/MindTrials Future Thinking/results/testing_MCAR.csv")
 
 #Multiple imputation  for multilevel data Using the pan package
-fml <- posExpBiasScale + negExpBiasScale + depressionScale + anxietyScale + selfEffScale + growthMindScale + optimismScale  ~ condition + session_int + condition*session_int + (1+session_int|participantId)
-imp <- panImpute(ittx, formula=fml, n.burn=10000, n.iter=100, m=100, seed=1234)
+fml <- posExpBiasScale + negExpBiasScale + depressionScale + anxietyScale + selfEffScale + growthMindScale + optimismScale + education + educationGrp   ~ condition + session_int + condition*session_int + (1+session_int|participantId)
+imp <- panImpute(x, formula=fml, n.burn=10000, n.iter=100, m=100, seed=1234)
 impList <- mitmlComplete(imp, print="all")
 #This si to summurize and plot the results of the imputation procedure
 #summary(imp)
@@ -216,7 +183,9 @@ ITT<-subset(p, ittSample==1)
 Completers<-subset(p, txCompSample==1)
 ittx<-x[which(x$participantId %in% ITT$participantId), ]
 
-
+x<-ittx
+d<-read.csv("C://Users/mob3f/Documents/MindTrials Future Thinking/Clean data from Jeremy/FTmainDataDemogITT.csv")
+x<-merge(d[,-c(3,4)],x,by="participantId")
 
 #Get a global view on our data
 summary(x)
@@ -225,8 +194,8 @@ md.pattern(x)
 
 
 #Multiple imputation  for multilevel data Using the pan package
-fml <- posExpBiasScale + negExpBiasScale + depressionScale + anxietyScale + selfEffScale + growthMindScale + optimismScale  ~ condition + session_int + condition*session_int + (1+session_int|participantId)
-imp2 <- panImpute(ittx, formula=fml, n.burn=10000, n.iter=100, m=100, seed=1234)
+fml <- posExpBiasScale + negExpBiasScale + depressionScale + anxietyScale + selfEffScale + growthMindScale + optimismScale + education + educationGrp  ~ condition + session_int + condition*session_int + (1+session_int|participantId)
+imp2 <- panImpute(x, formula=fml, n.burn=10000, n.iter=100, m=100, seed=1234)
 impList2 <- mitmlComplete(imp2, print="all")
 
 
